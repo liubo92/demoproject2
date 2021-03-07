aee28a600c13634d4c5432d980604c7cdbe6032c
liubo92 <297871690@qq.com>
2021-03-07 17:21:52 +0800

    本地项目

---
+++ javademo/src/com/lb/demo/Demo01.java
@@ -0,0 +1,11 @@
+package com.lb.demo;
+
+/**
+ * @author lb
+ * @create 2021-03-07
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello world.....");
+    }
+}
