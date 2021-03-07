8e54c21dfd1f61391116a7bf6c28efcea368f76c
liubo92 <297871690@qq.com>
2021-03-07 20:32:24 +0800

    添加文件

---
+++ javademo/src/com/lb/demo/advance/Test.java
@@ -0,0 +1,11 @@
+package com.lb.demo.advance;
+
+/**
+ * @author lb
+ * @create 2021-03-07
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("advance");
+    }
+}
